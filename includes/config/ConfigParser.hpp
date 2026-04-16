265ca8cddaf31f0bf19fbd32f961e0a88fcb5fb7
VYAntares <endroahm@hotmail.com>
2026-04-16 23:10:53 +0000

    config hpp

--- includes/config/ConfigParser.hpp
+++ includes/config/ConfigParser.hpp
@@ -1,49 +1,21 @@
 #pragma once
 
-#include <string>
-#include <vector>
-#include <map>
+#include "ConfigStruct.hpp"
+
 #include <set>
 #include <fstream>
 
-struct LocationConfig {
-	std::string							prefix;
-	std::string							root;
-	std::string							index;
-	bool								autoindex;
-	std::set<std::string>				allowed_methods;
-	bool								has_redirect;
-	int									redirect_code;
-	std::string							redirect_url;
-	std::string							upload_store;
-	std::map<std::string, std::string>	cgi_handlers;
+class ConfigParser {
+	public:
+		ConfigParser(const std::string& filepath);
+		void parse();
+		const std::vector<Server>& getServers() const;
 
+	private:
+		std::vector<std::string>	_tokens;
+		std::vector<Server>			_servers;
+		std::ifstream				_file;
+		std::string					_filepath;
+		std::size_t					_pos;
 };
 
-struct ServerConfig {
-	std::string					host;
-	int							port;
-	std::string					server_name;
-	std::string					root;
-	std::string					index;
-	size_t						client_max_body;
-	std::map<int, std::string>	error_pages;
-	std::vector<LocationConfig>	locations;
-};
-
-class Config {
-public:
-	Config(const std::string& filepath);
-	void parse();
-	const std::vector<ServerConfig>& getServers() const;
-
-private:
-	std::vector<ServerConfig>	_servers;
-	std::string					_filepath;
-	std::ifstream				_file;
-	std::vector<std::string>	_tokens;
-	std::size_t					_pos;
-	
-};
-
-}
