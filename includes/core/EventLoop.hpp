885c8ad500dd5b8b25794a9c5e9ee05619ac945e
VYAntares <endroahm@hotmail.com>
2026-04-23 16:38:29 +0000

    class

--- includes/core/EventLoop.hpp
+++ includes/core/EventLoop.hpp
@@ -1,2 +1,16 @@
 #pragma once
 
+#include "../handlers/AcceptHandler.hpp"
+#include "IEventHandler.hpp"
+#include <sys/epoll.h>
+
+#define MAX_EVENTS 128
+
+class EventLoop.h {
+	public:
+		EventLoop();
+		~EventLoop();
+		looping();
+	private:
+		int	_epfd;
+}
